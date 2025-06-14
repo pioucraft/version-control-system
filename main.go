a1624cd1cf12c6898ef4cda3b462349958021e30
Nathan Gasser <hello@gougoule.ch>
2025-06-14 14:22:31 +0200

    update help menu

--- main.go
+++ main.go
@@ -10,7 +10,13 @@ import (
 func main() {
 	// if the first argument is commmit, then call the commit function with the flag -m/--message for commit message
 	if len(os.Args) < 2 || (os.Args[1] == "help") {
-		fmt.Println("Usage: vc commit -m <message>")
+		fmt.Println("Usage: vc <command> [options]")
+		fmt.Println("Commands:")
+		fmt.Println("  commit -m <message>        Commit changes with a message")
+		fmt.Println("  diff                       Show differences for the current commit")
+		fmt.Println("  cat <key> [-c <commitId>]  Read content for a key at a specific commit")
+		fmt.Println("  help                       Show this help message")
+
 		return
 	}
 	if os.Args[1] == "commit" {
